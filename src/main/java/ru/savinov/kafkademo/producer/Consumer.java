fe2737a025953c35f6178814e29e7ebc9c323622
savinov2 <savinov2@mail.ru>
2023-06-29 14:55:53 +0300

    Initial commit

---
+++ src/main/java/ru/savinov/kafkademo/producer/Consumer.java
@@ -0,0 +1,38 @@
+package ru.savinov.kafkademo.producer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ru.savinov.kafkademo.consumer.Producer.class);
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group2" );
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        KafkaConsumer<Integer,String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singleton("demo-topic"));
+
+        while(true){
+            ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord<Integer,String> record : records){
+                logger.info(record.key() + "key " + record.key() + " value " + record.value() + " partition " + record.partition() +
+                        " offset" + record.offset());
+            }
+        }
+    }
+}
---
+++ src/test/java/ru/savinov/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.savinov.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.savinov.kafkademo.consumer.Producer;
+import ru.savinov.kafkademo.producer.Consumer;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
